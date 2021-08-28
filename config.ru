8d9a978c9adc7dbf9a68d02c7f758f28ac281297
misono <ginpeimisonou@gmail.com>
2021-08-28 17:37:15 +0900

    記事作成機能の追加

---
+++ config.ru
@@ -0,0 +1,3 @@
+require_relative 'lib/memo'
+require './app'
+run Sinatra::Application
--- lib/memo.rb
+++ lib/memo.rb
@@ -10,4 +10,24 @@ class Memo
       JSON.parse(f.read, symbolize_names: true) # load を利用すると Rubocop の警告がでるため、文字列として読み込み、parse する
     end
   end
+
+  def initialize(title, content)
+    @id = create_id
+    @title = title
+    @content = content
+  end
+
+  def save
+    memos = Memo.all
+    memos << { id: @id, title: @title, content: @content }
+    open(PATH, 'w') { |f| JSON.dump(memos, f) }
+  end
+
+  private
+
+  def create_id
+    memos = Memo.all
+    memos.sort_by! { |memo| memo[:id] }
+    memos.last[:id] + 1
+  end
 end
